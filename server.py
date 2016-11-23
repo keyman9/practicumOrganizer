f3969aaacafd78f0d08e50b138017a74ccaaa473
Bryan Nosar <bmnosar@gmail.com>
2016-11-23 18:01:06 +0000

    Pushing small changes

--- server.py
+++ server.py
@@ -289,6 +289,17 @@ def loadStudents():
 
 @socketio.on('loadTeachers', namespace='/practica') 
 def loadTeachers():
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    print("LOAD TEAACHERS")
+    
     teachers = load_teachers()
     emit('loadTeachers', teachers)
     
